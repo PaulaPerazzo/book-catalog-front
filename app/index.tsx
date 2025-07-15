bea4cf882d610befcf1c6192ff46394778d9c92a
paulaperazzo <mpps@cin.ufpe.br>
2025-07-15 16:34:14 -0300

    feat/login

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,9 @@
 import { Text, View } from "react-native";
 import Signup from "./Signup";
+import Login from "./Login";
 
 export default function Index() {
   return (
-    <Signup/>
+    <Login/>
   );
 }
